9ef6b08c9db4b242f9aa8f29c7b8a5599fe129e4
K V Naresh Karthigeyan <130460278+nareshkarthigeyan@users.noreply.github.com>
2025-07-26 09:48:49 +0530

    Update revly.rb

--- revly.rb
+++ revly.rb
@@ -2,7 +2,7 @@ class Revly < Formula
   desc "Revly CLI - AI Code Review Assistant for commits, stashes and unstashes"
   homepage "https://github.com/nareshkarthigeyan/revly"
   url "https://github.com/nareshkarthigeyan/revly/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "78acd4d4f66dcb857caeedb37888f0a3931260316a68ae9826c601ddbebd105d"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "go" => :build
